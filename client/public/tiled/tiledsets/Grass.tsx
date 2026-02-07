b5143695936b84844ba6696ddd23ec4702fcce5a
Daniel <dacarreno.24@est.ucab.edu.ve>
2026-02-07 12:19:57 -0400

    refactor: arreglar foramato de mapa de tiled para que sea compatible

--- client/public/tiled/tiledsets/Grass.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Grass" tilewidth="32" tileheight="32" tilecount="20" columns="10">
- <grid orientation="orthogonal" width="32" height="32"/>
- <image source="Grass.png" width="160" height="32"/>
-</tileset>
