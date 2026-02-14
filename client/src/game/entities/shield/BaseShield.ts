f8f270c9651255e981c43768239e5a38463db536
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2026-02-14 12:03:56 -0400

    refactor: ajustar escudo y quitar su tipo sprite

--- client/src/game/entities/shield/BaseShield.ts
+++ client/src/game/entities/shield/BaseShield.ts
@@ -1,17 +1,14 @@
-export default abstract class BaseShield extends Phaser.GameObjects.Sprite {
+export default abstract class BaseShield extends Phaser.GameObjects.Rectangle {
     protected ownerId: number | null = null;
 
-    constructor(scene: Phaser.Scene, x: number, y: number, texture: string, width: number = 64, height: number = 64) {
-        super(scene, x, y, texture);
+    constructor(scene: Phaser.Scene, x: number, y: number, width: number = 64, height: number = 64) {
+        super(scene, x, y);
         this.setDisplaySize(width, height);
         scene.add.existing(this);
         scene.physics.add.existing(this, true);
         this.scene.events.emit("shield-created", this);
 
-        const body = this.body as Phaser.Physics.Arcade.StaticBody | null;
-        if (body) {
-            body.setSize(width, height, true);
-        }
+        //const body = this.body as Phaser.Physics.Arcade.StaticBody;
     }
 
     setOwnerId(id: number | null): void {
--- client/src/game/entities/shield/Shield.ts
+++ client/src/game/entities/shield/Shield.ts
@@ -2,7 +2,7 @@ import BaseShield from "./BaseShield.ts";
 
 export default class Shield extends BaseShield {
     constructor(scene: Phaser.Scene, x: number, y: number) {
-        super(scene, x, y, "", 64, 64);
-        this.setVisible(false);
+        super(scene, x, y, 48, 48);
+        this.setVisible(true);
     }
 }
